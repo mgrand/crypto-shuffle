3f1c3ff5b09b4fa6f0612f56c6916a43d671b740
Mark Grand <mark.grand@HCL.COM>
2017-03-26 19:17:50 -0400

    Added simpleTest

--- src/test/java/com/markgrand/cryptoShuffle/EncryptionValuesTest.java
+++ src/test/java/com/markgrand/cryptoShuffle/EncryptionValuesTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import java.util.BitSet;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for EcryptionsValues
@@ -16,19 +14,29 @@ import static org.junit.Assert.assertTrue;
 @SuppressWarnings("unused")
 public class EncryptionValuesTest {
     private final byte[] key = {0x39, (byte) 0xe4, 0x32, (byte) 0xa3, (byte) 0x89, 0x00, 0x24, (byte) 0x97};
-    private final byte[] plaintext1 = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    private final byte[] plaintext2 = {0x6c, (byte)0x95};
+    private final byte[] plaintext16 = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
             0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
     private final byte[] encrypted1 = {0x01, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
             0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x40, (byte) 0xe1, 0x02, (byte) 0xa3, (byte) 0x94,
             (byte) 0xb5, 0x06, 0x07, 0x08, (byte) 0xf9, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 
     @Test
+    public void simpleTest() {
+        final EncryptionValues ev1 = EncryptionValues.forEncryption(plaintext2, key);
+        assertNotNull(ev1);
+        final EncryptionValues ev2 = EncryptionValues.forDecryption(plaintext2, key);
+        assertNotNull(ev2);
+        System.out.println(ev1.toString());
+    }
+
+    @Test
     public void forEncryptionTest() {
-        EncryptionValues ev1 = EncryptionValues.forEncryption(plaintext1, key);
-        assertEquals(plaintext1.length, ev1.getPadLength());
+        EncryptionValues ev1 = EncryptionValues.forEncryption(plaintext16, key);
+        assertEquals(plaintext16.length, ev1.getPadLength());
         assertEquals(8, ev1.getTargetIndices().length);
         assertEquals(ev1.getEncryptedLength(), ev1.getTargetIndices()[2].length);
-        assertEquals(ev1.getEncryptedLength(), plaintext1.length + ev1.getPadLength());
+        assertEquals(ev1.getEncryptedLength(), plaintext16.length + ev1.getPadLength());
         StringBuilder builder = new StringBuilder();
 
         BitSet bits = new BitSet(ev1.getEncryptedLength());
@@ -47,7 +55,7 @@ public class EncryptionValuesTest {
         assertEquals((encrypted1.length - 1) / 2, ev1.getPadLength());
         assertEquals(8, ev1.getTargetIndices().length);
         assertEquals(ev1.getEncryptedLength(), ev1.getTargetIndices()[2].length);
-        assertEquals(ev1.getEncryptedLength(), plaintext1.length + ev1.getPadLength());
+        assertEquals(ev1.getEncryptedLength(), plaintext16.length + ev1.getPadLength());
         StringBuilder builder = new StringBuilder();
 
         BitSet bits = new BitSet(ev1.getEncryptedLength());
@@ -62,7 +70,7 @@ public class EncryptionValuesTest {
 
     @Test
     public void consistencyTest() {
-        EncryptionValues ev1 = EncryptionValues.forEncryption(plaintext1, key);
+        EncryptionValues ev1 = EncryptionValues.forEncryption(plaintext16, key);
         EncryptionValues ev2 = EncryptionValues.forDecryption(encrypted1, key);
         assertArrayEquals(ev1.getTargetIndices(), ev2.getTargetIndices());
     }
