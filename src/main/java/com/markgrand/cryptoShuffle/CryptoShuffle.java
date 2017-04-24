aa369a2b1dd42818f2d0a4c0dabb9fddfa0188d2
Mark Grand <mgrand@markgrand.com>
2017-04-23 22:15:30 -0400

    Updated the class comment to better describe the algorithm.

--- src/main/java/com/markgrand/cryptoShuffle/CryptoShuffle.java
+++ src/main/java/com/markgrand/cryptoShuffle/CryptoShuffle.java
@@ -3,63 +3,95 @@ package com.markgrand.cryptoShuffle;
 import java.util.Random;
 
 /**
- * <p>Implements a highly secure algorithm with the following properties:</p>
+ * <p>This is a symmetric encryption algorithm for use in applications like
+ * protecting the secrecy of blockchain contents where the encryption needs
+ * to be very strong. The algorithm should has these properties:</p>
  * <ul>
- * <li>Longer encrypted texts should require more effort to crack than shorter texts.</li>
- * <li>It should be necessary to decrypt the entire text at once, rather than  being possible to decrypt in pieces
- * as with a block cypher.</li>
- * <li>There should be no upper limit on the key length.</li>
- * <li>One of the challenges of cracking the encryption should be that there
- * will be multiple possible solutions that look reasonable and no clue
- * as to which is correct.</li>
+ * <li>Longer encrypted texts require more effort to crack than shorter texts.</li>
+ * <li>It is necessary to decrypt the entire text at once, rather than decrypt in pieces as with a block cypher.</li>
+ * <li>There is no upper limit on the key length.</li>
+ * <li>One of the challenges of cracking the encryption is that there will be multiple solutions that look reasonable
+ * and no clue as to which is correct.</li>
  * </ul>
- * <p>The algorithm implemented in this package is based on doing a random shuffle of the ones and zeros in a plaintext.
- * The actual order of the shuffle is based on the encryption key.</p>
- * <p>Here is a high-level description of the algorithm:</p>
+ * <p>
+ * The algorithm implemented in this package is based on doing a random
+ * shuffle of the ones and zeros in a plaintext. The actual order of the
+ * shuffle is based on the encryption key.</p>
+ * <p>
+ * Here is a high-level description of the encryption algorithm based on
+ * the bouncycastle library:</p>
  * <ol>
- * <li>Inputs to the algorithm are a plaintext message that is a sequence of bytes and a key that is an
- * arbitrary sequence of bytes.</li>
- * <li>Compute a SHA3-512 hash of the key.</li>
- * <li>Use the hash as the seed for a pseudo-random sequence of numbers. The
+ * <li>Inputs to the algorithm are a plaintext message that is a sequence of
+ * bytes and a key that is an arbitrary sequence of bytes.<br/><br/></li>
+ * <li>Compute a SHA512 hash of the key.<br/><br/></li>
+ * <li>The purpose of this step is to add random extraneous bits to ensure
+ * that a brute force attempt to decrypt the encrypted text will result
+ * in multiple candidates for the plaintext that will be wrong but
+ * appear to be a reasonable solution.<br/><br/></li>
+ * <li>Using a separate random number that is in no way dependent on the key
+ * we are using, append random bytes to the plaintext to double the
+ * length of the plaintext.
+ * <p>Use the hash as the seed for a pseudo-random sequence of numbers. The
  * following steps will be based on this sequence. The decryption
  * operation will consist of performing the following steps in reverse
  * using the same sequence of pseudo-random numbers.
  * <p>For interoperability, all implementations of this encryption
  * algorithm will need to use the same pseudo-random number generator.
  * The DigestRandomGenerator class from the Bouncycastle library is used
- * for this purpose. There is a <a href="https://www.hgi.rub.de/media/nds/veroeffentlichungen/2013/03/25/paper_2.pdf">
- * paper that includes an analysis of this pseudo-random number generator.</a></p></li>
- * <li>Append to the plaintext bytes containing the length of the plaintext
- * as an unsigned binary integer. For consistency, the bytes should be
- * appended in big-endian order (most significant byte first, least
- * significant byte last). The length of the integer should be the
- * minimum number of bytes needed to represent the value.
- * <p>The reason for not using a fixed number of bytes to represent the
- * length is to avoid having a high-order byte that is likely to be zero
- * and thereby provide a clue as to location of the length value.</p></li>
- * <li>Append a byte to the plaintext that contains the number of bytes
- * appended to the plaintext in the previous step.  The actual value
- * stored in this byte should be the sum of the number of bytes plus a
- * random value, ignoring overflow. The reason for adding the random
- * number is to avoid having a predictable value in this position.</li>
- * <li>This step has two purposes. Firstly we want to avoid giving any clue
- * about the plaintext from the relative number of ones and zeros in the
- * encrypted message. Secondly, we want to add extraneous bits to ensure
- * that a brute force attempt to decrypt the encrypted text will result
- * in multiple candidates for the plaintext that will be wrong but
- * appear to be a reasonable solution.
- * <p>
- * <p>Compute a random number r that is between _n_ and _2n_, where _n_ is
- * the current length of the plaintext. Using a separate random number
- * generator that is independent of the one we are using for every other
- * step, append _r_ random bytes to the plaintext. If the total number
- * of ones and zeros in the plaintext is not equal, then using the same
- * independent pseudo random number generator, invert randomly selected
- * bits in the appended bytes until the number of ones and zeros in the
- * plaintext is equal.</p></li>
+ * for this purpose. There is a paper that includes an analysis of this
+ * pseudo-random number generator at
+ * <a href="https://www.hgi.rub.de/media/nds/veroeffentlichungen/2013/03/25/paper_2.pdf">https://www.hgi.rub.de/media/nds/veroeffentlichungen/2013/03/25/paper_2.pdf</href>.
+ * <br/><br/></li>
  * <li>Perform a shuffle of the plaintext based on pseudo-random numbers.</li>
  * </ol>
- *
+ * There is a wrinkle to the algorithm that is not mentioned above. The
+ * pseudo-random numbers are generated from a SHA512 has of the key. This
+ * is a 512 bit hash value. If we use the entire key to compute the the
+ * hash value and then compute the pseudo-random numbers, then no matter
+ * how long the given encryption key is, the effective length of the key is
+ * limited 512 bits.
+ * <p>
+ * While 512 bits is a respectable key size, limiting the effective key
+ * length to 512 bits is a problem. The problem is that it weakens the goal
+ * of this algorithm being harder to crack for longer texts.
+ * <p>
+ * The number of possible shuffles of the bits in a long plaintext is
+ * limited by the effective key length. If it were the case that the
+ * effective key length was limited to 512 bits, then there would be
+ * shuffles of longer plaintexts that could be ruled out as not being
+ * possible to generate from an 512 bit key. For this reason, the way that
+ * we use the key is modified for long keys with long plaintexts, so that
+ * the effective length of the key is as unlimited as the given keys.
+ * <p>
+ * If the key is longer than 256 bytes and the plaintext is longer than 128
+ * bytes then the algorithm uses the key in a more elaborate way.
+ * <p>
+ * The plaintext is divided into groups of 64 bytes with the possibility of
+ * the last group being less than 64 bytes. The key is divided into groups
+ * of 128 bytes with the possibility of the last group being less than 128
+ * bytes.
+ * <p>
+ * If this yields an equal number of key and plaintext groups, then this is
+ * how the key is used: The SHA512 hash for the first key group is computed
+ * and then used as the seed to generate pseudo-random numbers that determine
+ * the shuffle destination of the bits in the first plaintext group. The
+ * second key group is then combined with the current seed value to produce
+ * a new SHA512 hash. This new hash is used as the seed to generate
+ * pseudo-random numbers that determine the shuffle destination of the bits
+ * in the second plaintext group. This procedure continues to the end of
+ * the groups.
+ * <p>
+ * If there are fewer key groups than plaintext groups, then after the last
+ * key group has been incorporated into the seed value that seed is used to
+ * generate pseudo-random numbers for the rest of the groups in the plaintext.
+ * If there are more key groups than plaintext groups, then the size of the
+ * key groups is increased so that the number of key groups will be equal
+ * to the number of plaintext groups.
+ * <p>
+ * As mentioned above, the encryption algorithm is implemented using the
+ * Bouncycastle library. This implementation of Crypto-Shuffle is written
+ * in Java. There is a C# implementation of the Bouncycastle library, so
+ * perhaps there will be a C# implementation of Crypto-Shuffle.
  * @author Mark Grand
  */
 @SuppressWarnings({"unused", "WeakerAccess"})
