00d788a88e2249edcba6a4f4cc3c13df0ab34c0f
Mark Grand <mark.grand@HCL.COM>
2017-07-05 14:35:54 -0400

    New File

---
+++ src/main/java/com/markgrand/cryptoShuffle/JsonUtil.java
@@ -0,0 +1,8 @@
+package com.markgrand.cryptoShuffle;
+
+/**
+ * <p>Utility class for converting instances of classes in this package to and from JSON.</p>
+ * Created by mark.grand on 7/5/2017.
+ */
+public class JsonUtil {
+}
