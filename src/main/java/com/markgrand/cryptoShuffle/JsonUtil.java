3c9ae622be94800f8601e95f776605d2337883d5
Mark Grand <mark.grand@HCL.COM>
2017-07-05 16:28:18 -0400

    Added some code.

--- src/main/java/com/markgrand/cryptoShuffle/JsonUtil.java
+++ src/main/java/com/markgrand/cryptoShuffle/JsonUtil.java
@@ -1,8 +1,17 @@
 package com.markgrand.cryptoShuffle;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * <p>Utility class for converting instances of classes in this package to and from JSON.</p>
  * Created by mark.grand on 7/5/2017.
  */
 public class JsonUtil {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static JsonNode keyShardSetToJson(final KeyShardSet keyShardSet) {
+
+        return objectMapper.valueToTree(keyShardSet);
+    }
 }
