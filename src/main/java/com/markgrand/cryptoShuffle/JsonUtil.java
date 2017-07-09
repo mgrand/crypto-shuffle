83ff11fba118adadb588c1074281d165dad853f5
Mark Grand <mgrand@markgrand.com>
2017-07-09 09:39:21 -0400

    Added more code.

--- src/main/java/com/markgrand/cryptoShuffle/JsonUtil.java
+++ src/main/java/com/markgrand/cryptoShuffle/JsonUtil.java
@@ -8,6 +8,13 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Map;
 
 /**
  * <p>Utility class for converting instances of classes in this package to and from JSON.</p>
@@ -33,6 +40,7 @@ public class JsonUtil {
         public KeyShardSetSerializer() {
             this(null);
         }
+
         public KeyShardSetSerializer(Class<KeyShardSet> t) {
             super(t);
         }
@@ -41,10 +49,11 @@ public class JsonUtil {
         public void serialize(KeyShardSet value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
             jsonGenerator.writeStartObject();
             jsonGenerator.writeStringField("version", "1.0");
+            jsonGenerator.writeStringField("encryptionAlgorithm", "RSA");
             jsonGenerator.writeStringField("uuid", value.getGuid().toString());
             jsonGenerator.writeNumberField("shardCount", value.getShardCount());
             jsonGenerator.writeArrayFieldStart("groups");
-            for (KeyShardSet.KeyShardGroup group: value.getGroups()) {
+            for (KeyShardSet.KeyShardGroup group : value.getGroups()) {
                 jsonGenerator.writeObject(group);
             }
             jsonGenerator.writeEndArray();
@@ -68,8 +77,33 @@ public class JsonUtil {
             jsonGenerator.writeStartObject();
             jsonGenerator.writeNumberField("quorumSize", value.getQuorumSize());
             jsonGenerator.writeArrayFieldStart("keyMap");
+            serializeGroupPublicKeys(value, jsonGenerator);
             jsonGenerator.writeEndArray();
             jsonGenerator.writeEndObject();
         }
+
+        private void serializeGroupPublicKeys(KeyShardSet.KeyShardGroup value, JsonGenerator jsonGenerator) throws IOException {
+            for (PublicKey publicKey : value.getKeys()) {
+                jsonGenerator.writeStartObject();
+                final String encodedPublicKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+                jsonGenerator.writeStringField("publicKey", encodedPublicKey);
+                jsonGenerator.writeArrayFieldStart("shards");
+                final Map<Integer, byte[]> shards = value.getShardsForKey(publicKey);
+                for (Map.Entry<Integer, byte[]> shard : shards.entrySet()) {
+                    jsonGenerator.writeStartObject();
+                    jsonGenerator.writeNumberField("shardPosition", shard.getKey());
+                    final String base64EncodedEncryptedShard = Base64.getEncoder().encodeToString(shard.getValue());
+                    jsonGenerator.writeStringField("encryptedShard", base64EncodedEncryptedShard );
+                    jsonGenerator.writeEndObject();
+                }
+                jsonGenerator.writeEndArray();
+                jsonGenerator.writeEndObject();
+            }
+        }
+    }
+
+    private PublicKey X509EncodedKeySpecToRsaPublicKey(final byte[] bytes)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(bytes));
     }
 }
