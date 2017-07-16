612094aef630de7ba2008e33a2e956c216ff400c
Mark Grand <mark.grand@HCL.COM>
2017-07-16 13:11:32 -0400

    New File

---
+++ src/main/java/com/markgrand/cryptoShuffle/keyShard/SymmetricEncryptionAlgorithms.java
@@ -0,0 +1,10 @@
+package com.markgrand.cryptoShuffle.keyShard;
+
+/**
+ * <p>Enumeration of supported symmetric encryption algorithms.
+ * </p>
+ * Created by mark.grand on 7/16/2017.
+ */
+public enum SymmetricEncryptionAlgorithms {
+    AES;
+}
