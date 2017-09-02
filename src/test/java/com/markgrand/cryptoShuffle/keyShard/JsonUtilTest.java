aacc1febb575fc20961cfc4723534998c8cf95d5
Mark Grand <mgrand@markgrand.com>
2017-09-02 10:26:27 -0400

    Removed some dead code.

--- src/test/java/com/markgrand/cryptoShuffle/keyShard/JsonUtilTest.java
+++ src/test/java/com/markgrand/cryptoShuffle/keyShard/JsonUtilTest.java
@@ -6,20 +6,19 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.markgrand.cryptoShuffle.AbstractTest;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.security.KeyPair;
-import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * <p>Unit tests for JsonUtil.</p>
@@ -31,8 +30,6 @@ public class JsonUtilTest extends AbstractTest implements JsonSchemaConstants {
     private KeyShardSet keyShardSet;
 
     private static ObjectMapper objectMapper;
-
-    private final Map<PublicKey, PrivateKey> keyDictionary = new HashMap<>();
 
     @BeforeClass
     public static void initSchema() throws Exception {
@@ -58,18 +55,10 @@ public class JsonUtilTest extends AbstractTest implements JsonSchemaConstants {
             final Set<PublicKey> publicKeys5 = keyPairs5.stream().map(KeyPair::getPublic).collect(Collectors.toSet());
             final Set<PublicKey> publicKeys3 = keyPairs3.stream().map(KeyPair::getPublic).collect(Collectors.toSet());
             keyShardSet = builder.addKeyGroup(2,  publicKeys5).addKeyGroup(3,  publicKeys3).build(key4800);
-//        addToKeyDictionary(keyPairs3);
-//        addToKeyDictionary(keyPairs5);
         } catch (Throwable e) {
             System.err.println("Before");
             e.printStackTrace();
             throw e;
-        }
-    }
-
-    private void addToKeyDictionary(Set<KeyPair> keyPairs3) {
-        for (KeyPair keyPair : keyPairs3) {
-            keyDictionary.put(keyPair.getPublic(), keyPair.getPrivate());
         }
     }
 
